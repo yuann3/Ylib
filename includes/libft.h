0fd5f9367ece7dc2c1388caa7e64330c11e443a6
Yiyuan <etherealdt@gmail.com>
2024-04-16 15:09:41 +0800

    add libft.h

---
+++ includes/libft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yiyli <etherealdt@gmail.com>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/16 14:56:51 by yiyli             #+#    #+#             */
+/*   Updated: 2024/04/16 15:09:12 by yiyli            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+
+char	**ft_split(char *str, char *charset);
+
+#endif
