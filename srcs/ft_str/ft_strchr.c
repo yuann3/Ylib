8c11b819e43a1e08b7e8662b28fe5e168662c31f
Yiyuan <etherealdt@gmail.com>
2024-04-17 19:47:32 +0800

    add documentation to the ft_strchr function

--- srcs/ft_str/ft_strchr.c
+++ srcs/ft_str/ft_strchr.c
@@ -6,12 +6,23 @@
 /*   By: yiyli <etherealdt@gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 14:06:25 by yiyli             #+#    #+#             */
-/*   Updated: 2024/04/17 17:15:42 by yiyli            ###   ########.fr       */
+/*   Updated: 2024/04/17 18:51:21 by yiyli            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/**
+ * @file ft_strchr.c
+ * @brief Locates the first occurrence of c in string s.
+ * @param s String to be scanned.
+ * @param c Character to be located.
+ * @return Pointer to the first occurrence of c in s, or NULL.
+ * 
+ * The search includes the terminating null character. If c is
+ * found, a pointer to c in s is returned. If c is not found,
+ * the function returns NULL.
+ */
 char	*ft_strchr(const char *s, int c)
 {
 	int	i;
