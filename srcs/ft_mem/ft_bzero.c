57b7af50a074800b86087af042a0a92e3c23ea42
Yiyuan <etherealdt@gmail.com>
2024-04-26 17:37:05 +0800

    yo bruh we just finished all ft_mem functions, now we can move on to ft_lst

--- srcs/ft_mem/ft_bzero.c
+++ srcs/ft_mem/ft_bzero.c
@@ -6,12 +6,19 @@
 /*   By: yiyli <etherealdt@gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 17:10:32 by yiyli             #+#    #+#             */
-/*   Updated: 2024/04/26 17:16:16 by yiyli            ###   ########.fr       */
+/*   Updated: 2024/04/26 17:35:57 by yiyli            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/**
+ * @file ft_bzero.c
+ * @brief Sets the first n bytes of the block of memory pointed by s to zero.
+ *
+ * @param s Pointer to the block of memory to set to zero.
+ * @param n Number of bytes to set to zero.
+ */
 void	ft_bzero(void *s, size_t n)
 {
 	unsigned char	*p;
--- srcs/ft_mem/ft_calloc.c
+++ srcs/ft_mem/ft_calloc.c
@@ -6,12 +6,19 @@
 /*   By: yiyli <etherealdt@gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 17:19:08 by yiyli             #+#    #+#             */
-/*   Updated: 2024/04/26 17:31:05 by yiyli            ###   ########.fr       */
+/*   Updated: 2024/04/26 17:36:55 by yiyli            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/**
+ * @file ft_calloc.c
+ * @brief Sets the first n bytes of the block of memory pointed by s to zero.
+ *
+ * @param s Pointer to the block of memory to set to zero.
+ * @param n Number of bytes to set to zero.
+ */
 void	*ft_calloc(size_t count, size_t size)
 {
 	size_t	total;
