e8f488ca6dc2f4878f484dd24c5c65024e73db9c
Yiyuan <etherealdt@gmail.com>
2024-05-27 21:46:53 +0800

    fix error that strmapi does not allocate the good size so the \0 test may be false

--- srcs/ft_str/ft_strmapi.c
+++ srcs/ft_str/ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: yiyli <etherealdt@gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 13:34:35 by yiyli             #+#    #+#             */
-/*   Updated: 2024/05/19 14:18:18 by yiyli            ###   ########.fr       */
+/*   Updated: 2024/05/27 21:38:35 by yiyli            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,24 +22,25 @@
  * @return char* The new string resulting from successive applications of 'f',
 	or NULL if an error occurs.
  */
+
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-	char			*new_str;
-	size_t			len;
+	char			*str_new;
+	size_t			s_len;
 	unsigned int	i;
 
-	i = 0;
-	len = 0;
 	if (s == NULL || f == NULL)
 		return (NULL);
-	new_str = (char *)malloc(len + 1);
-	if (new_str == NULL)
+	s_len = ft_strlen(s);
+	str_new = (char *)malloc(sizeof(char) * (s_len + 1));
+	if (str_new == NULL)
 		return (NULL);
-	while (s[i] != '\0')
+	i = 0;
+	while (i < s_len)
 	{
-		new_str[i] = f(i, s[i]);
+		str_new[i] = f(i, s[i]);
 		i++;
 	}
-	new_str[i] = '\0';
-	return (new_str);
+	str_new[i] = '\0';
+	return (str_new);
 }
