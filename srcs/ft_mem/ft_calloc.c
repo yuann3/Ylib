f6667706dfc2581357b01c0e72b942a75c3d3ee9
Yiyuan Li <yy@eyuan.me>
2025-11-20 14:41:31 +0800

    improve implementatoin

--- srcs/ft_mem/ft_calloc.c
+++ srcs/ft_mem/ft_calloc.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
+/*                                                          :::      ::::::   */
 /*   ft_calloc.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: yiyli <etherealdt@gmail.com>               +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/26 17:19:08 by yiyli             #+#    #+#             */
-/*   Updated: 2024/05/27 20:35:20 by yiyli            ###   ########.fr       */
+/*                                                  +:+ +:+           +:+     */
+/*   By: yiyuli <yy@eyuan.me>                     +#+  +:+         +#+        */
+/*                                              +#+#+#+#+#+      +#+          */
+/*   Created: 2025/11/20 13:43:09 by yiyuli           #+#      #+#            */
+/*   Updated: 2025/11/20 13:53:14 by yiyuli         ###      ########.fr      */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,12 @@
 void	*ft_calloc(size_t count, size_t size)
 {
 	void	*ptr;
+	size_t	total_size;
 
-	ptr = malloc(count * size);
+	total_size = count * size;
+	if (count != 0 && total_size / count != size)
+		return (NULL);
+	ptr = malloc(total_size);
 	if (ptr)
 		ft_bzero(ptr, count * size);
 	return (ptr);
--- srcs/ft_str/ft_itoa.c
+++ srcs/ft_str/ft_itoa.c
@@ -1,31 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
+/*                                                          :::      ::::::   */
 /*   ft_itoa.c                                          :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: yiyli <etherealdt@gmail.com>               +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/26 13:48:49 by yiyli             #+#    #+#             */
-/*   Updated: 2024/05/21 16:01:37 by yiyli            ###   ########.fr       */
+/*                                                  +:+ +:+           +:+     */
+/*   By: yiyuli <yy@eyuan.me>                     +#+  +:+         +#+        */
+/*                                              +#+#+#+#+#+      +#+          */
+/*   Created: 2025/11/20 14:33:58 by yiyuli           #+#      #+#            */
+/*   Updated: 2025/11/20 14:38:41 by yiyuli         ###      ########.fr      */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/**
- * @brief Calculates the number of digits in an integer.
- *
- * @param n The integer.
- * @return int The number of digits in the integer.
- */
 static int	ft_numlen(int n)
 {
 	int	len;
 
 	len = 0;
 	if (n <= 0)
-		len = 1;
-	while (n != 0)
+	{
+		len++;
+		n = -n;
+	}
+	while (n > 0)
 	{
 		n /= 10;
 		len++;
@@ -33,40 +30,29 @@ static int	ft_numlen(int n)
 	return (len);
 }
 
-static void	ft_fill_str(char *str, int n, int len)
-{
-	int	sign;
-
-	sign = 1;
-	if (n < 0)
-		sign = -1;
-	str[len] = '\0';
-	while (len--)
-	{
-		str[len] = (n % 10) * sign + '0';
-		n /= 10;
-	}
-	if (sign == -1)
-		str[0] = '-';
-}
-
-/**
- * @file ft_itoa.c
- * @brief Converts an integer to a string.
- *
- * @param n The integer.
- * @return char* The string representation of the integer,
-	or NULL if an error occurs.
- */
 char	*ft_itoa(int n)
 {
 	char	*str;
 	int		len;
+	long	nbr;
 
+	nbr = n;
 	len = ft_numlen(n);
 	str = (char *)malloc(sizeof(char) * (len + 1));
 	if (!str)
 		return (NULL);
-	ft_fill_str(str, n, len);
+	str[len--] = '\0';
+	if (nbr == 0)
+		str[0] = '0';
+	if (nbr < 0)
+	{
+		str[0] = '-';
+		nbr = -nbr;
+	}
+	while (nbr > 0)
+	{
+		str[len--] = (nbr % 10) + '0';
+		nbr /= 10;
+	}
 	return (str);
 }
